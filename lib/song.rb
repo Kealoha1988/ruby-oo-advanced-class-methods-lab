2b22e63d1b0c8fd07a285e3b888329d29b8c177f
Kealoha1988 <kealohaabe1988@gmail.com>
2020-11-19 03:35:23 -0500

    commit tree 53c9b1b8b722117315762b82c52f56bbfc6ce819

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,64 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end 
+
+  def self.create_by_name(name)
+    song = Song.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(thing)
+    Song.all.find{|huh| huh.name == thing}
+  end
+
+  def self.find_or_create_by_name(thing)
+    self.find_by_name(thing) or self.create_by_name(thing)
+  end
+  
+  def self.alphabetical
+    @@all.sort_by{|x| x.name}
+  end
+
+  def self.new_from_filename(thing)
+    song = Song.new
+    artist_name = name
+    array = thing.split(/[-.]/)
+    song.artist_name = array[0].strip
+    r = array[1].strip
+    song.name = r
+    song
+  end
+
+  def self.create_from_filename(thing)
+    song = Song.new
+    artist_name = name
+    song.save
+    array = thing.split(/[-.]/)
+    song.artist_name = array[0].strip
+    r = array[1].strip
+    song.name = r
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+
   def self.all
     @@all
   end
